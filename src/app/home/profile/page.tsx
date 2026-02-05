015a050d904475839d3741294b907192cf36279d
Caio Santos <caiosantos.dev@outlook.com>
2026-02-05 19:29:03 -0300

    fix

--- src/app/home/profile/page.tsx
+++ src/app/home/profile/page.tsx
@@ -1,10 +1,12 @@
+"use client"
+
 import { ProfileMenu } from "@/components/pages/profile-menu/ProfileMenu";
 import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-    title: "Pasbem | Perfil",
-    description: "This is Next.js Home for TailAdmin Perfil Template",
-};
+// export const metadata: Metadata = {
+//     title: "Pasbem | Perfil",
+//     description: "This is Next.js Home for TailAdmin Perfil Template",
+// };
 
 export default function Profile() {
     return (
